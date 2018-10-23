063e37c0eaa6fab62818fcee50d953ead828f91e
zhangzhanyi <zhangzhanyi@i-focusing.com>
2018-10-23 10:47:47 +0800

    添加工具类将markdown文档转为纯文本

--- src/main/java/com/zzy/mywebsitebackend/Util/Util.java
+++ src/main/java/com/zzy/mywebsitebackend/Util/Util.java
@@ -1,35 +1,52 @@
 package com.zzy.mywebsitebackend.Util;
 
+import com.vladsch.flexmark.ast.Node;
+import com.vladsch.flexmark.ast.util.TextCollectingVisitor;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.profiles.pegdown.Extensions;
+import com.vladsch.flexmark.profiles.pegdown.PegdownOptionsAdapter;
+import com.vladsch.flexmark.util.options.DataHolder;
+import com.vladsch.flexmark.util.options.MutableDataSet;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 
 import java.io.*;
 
 @Slf4j
 public class Util {
 
-    public static byte[] ObjectToByte(Object obj) {
-        if(obj == null) return null;
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        try (ObjectOutputStream out = new ObjectOutputStream(baos)){
-            out.writeObject(obj);
-        } catch (IOException e) {
-            log.error("ObjectToByte error!"+e.getMessage());
-        }
-        return baos.toByteArray();
+    public static  String Markdown2PlainText(String markdown){
+        return Markdown2PlainTextUtil.Markdown2PlainText(markdown);
     }
 
-    public static <T> T ByteToObject(byte[] bytes){
-        if(bytes == null) return null;
-        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
-        try(
-                ObjectInputStream in = new ObjectInputStream(bais)
-        ){
-            return (T)in.readObject();
-        }catch (IOException e) {
-            log.error("ByteToObject error!", e.getMessage());
-        } catch (ClassNotFoundException e) {
-            log.error("ByteToObject error!", e.getMessage());
+    private static class Markdown2PlainTextUtil{
+        private static final DataHolder OPTIONS = PegdownOptionsAdapter.flexmarkOptions(
+                Extensions.ALL
+        );
+
+        private static final MutableDataSet FORMAT_OPTIONS = new MutableDataSet();
+        static {
+            // copy extensions from Pegdown compatible to Formatting
+            FORMAT_OPTIONS.set(Parser.EXTENSIONS, OPTIONS.get(Parser.EXTENSIONS));
         }
-        return null;
+
+        private static final Parser PARSER = Parser.builder(OPTIONS).build();
+
+        // use the PARSER to parse and RENDERER to parse pegdown indentation rules and render CommonMark
+
+        /**
+         * markdown文档转为纯文本
+         * @param markdown 需要转换的markdown文档
+         * @return
+         */
+        private static  String Markdown2PlainText(String markdown){
+            if(StringUtils.isEmpty(markdown)){
+                return markdown;
+            }
+            Node document = PARSER.parse(markdown);
+            TextCollectingVisitor textCollectingVisitor = new TextCollectingVisitor();
+            String text = textCollectingVisitor.collectAndGetText(document);
+            return text;
+        }
     }
 }
